082d83a6de8031607e80fe6b748b206fdb32cac4
AswinJD5 <aswinjd5@gmail.com>
2024-10-03 01:09:46 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, Response
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import google.generativeai as genai
+from PIL import Image
+import os
+# from dotenv import load_dotenv
+from flask_socketio import SocketIO, emit
+
+# load_dotenv()
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# Initialize the webcam to capture video
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# Initialize the HandDetector class with the given parameters
+detector = HandDetector(staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.7, minTrackCon=0.5)
+
+genai.configure(api_key= 'AIzaSyBQk7Saxz2x4zyEhLTUforesq1oc6igQnY')
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+prev_pos = None
+canvas = None
+
+def getHandInfo(img):
+    hands, img = detector.findHands(img, draw=False, flipType=True)
+    if hands:
+        hand = hands[0]
+        lmList = hand["lmList"]
+        fingers = detector.fingersUp(hand)
+        return fingers, lmList
+    else:
+        return None
+
+def draw(info, prev_pos, canvas, img):
+    fingers, lmList = info
+    current_pos = None
+    if fingers == [0, 1, 0, 0, 0]:
+        current_pos = lmList[8][0:2]
+        if prev_pos is None: prev_pos = current_pos
+        cv2.line(canvas, tuple(current_pos), tuple(prev_pos), (255, 0, 255), 10)
+    elif fingers == [1, 0, 0, 0, 0]:
+        canvas = np.zeros_like(img)
+    return current_pos, canvas
+
+def sendToAI(model, canvas, fingers):
+    if fingers == [1, 1, 1, 1, 0]:
+        pil_image = Image.fromarray(canvas)
+        response = model.generate_content(["Solve this math problem", pil_image])
+        return response.text
+    return ""
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen_frames():
+    global prev_pos, canvas
+    while True:
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+
+        if canvas is None:
+            canvas = np.zeros_like(img)
+
+        info = getHandInfo(img)
+        if info:
+            prev_pos, canvas = draw(info, prev_pos, canvas, img)
+            output_text = sendToAI(model, canvas, info[0])
+            if output_text:
+                print("AI Output:", output_text)
+                socketio.emit('ai_output', {'text': output_text})
+                return ""
+
+        image_combined = cv2.addWeighted(img, 0.7, canvas, 0.3, 0)
+        ret, buffer = cv2.imencode('.jpg', image_combined)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
